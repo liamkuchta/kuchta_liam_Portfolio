b07dc8738607354da64c943e5475b25d27007ccb
liamkuchta <liamkuchta@gmail.com>
2026-02-27 13:41:35 -0500

    update js
    updated contact form js and main js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 import {burgerMenu} from "./modules/burger-menu.js";
 import {scrollTriggers} from "./modules/scroll-triggers.js";
 import {sideScroller} from "./modules/side-scroller.js";
+import {initContactForm} from "./modules/contact-form.js";
 
-burgerMenu();
-scrollTriggers();
-sideScroller();
+// initialize when DOM is ready to ensure elements exist
+document.addEventListener('DOMContentLoaded', () => {
+  burgerMenu();
+  scrollTriggers();
+  sideScroller();
+  initContactForm();
+});
 
 
 // (() => { 
